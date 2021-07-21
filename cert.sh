afac9240de6fc8337e2110e820b0c5c08e7fed1d
dickmao <none>
2021-07-21 07:54:56 -0400

    renew cert

--- cert.sh
+++ cert.sh
@@ -7,3 +7,6 @@ cd ./cert
 certbot certonly -n --register-unsafely-without-email -d $DOMAIN -d $WILDCARD --dns-route53 --logs-dir ./log --config-dir ./config --work-dir ./work --agree-tos
 rsync -vaLzSHe ssh ./config/live/commandlinesystems.com/privkey.pem $BITNAMI:/opt/bitnami/nginx/conf/bitnami/certs/server.key
 rsync -vaLzSHe ssh ./config/live/commandlinesystems.com/fullchain.pem $BITNAMI:/opt/bitnami/nginx/conf/bitnami/certs/server.crt
+
+# the above also *renews* the cert, after which,
+# sudo /opt/bitnami/ctlscript.sh restart nginx
