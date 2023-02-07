f03419d9d48f85379fc8058098e769b6d0216197
dickmao <none>
2023-02-06 22:45:54 -0500

    whitespace

--- pipe.php
+++ pipe.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function live_curl($url, $filename) {
     $ch = curl_init($url);
@@ -10,7 +10,7 @@ function live_curl($url, $filename) {
     fclose($fp);
     return $ch;
 }
-    
+
 # $nominal_target = basename($_SERVER['REQUEST_URI']);
 # list($nominal_suffix, $package_name) = array_map("strrev", explode("-", strrev($nominal_target), 2));
 # list($suffix) = array_map("strrev", explode(strrev("snapshot"), strrev($nominal_suffix), 2));
@@ -21,10 +21,10 @@ list(, $package_name) = array_map("strrev", explode("-", strrev($target), 2));
 $cache = dirname($_SERVER['DOCUMENT_ROOT']) . "/tmp/targets";
 if (!is_dir($cache)) {
     if (!mkdir($cache, 0755, true)) {
-	header("Content-Type: text/plain");
-	echo "$cache\n";
-	echo error_get_last()['message'];
-	exit();
+        header("Content-Type: text/plain");
+        echo "$cache\n";
+        echo error_get_last()['message'];
+        exit();
     }
 }
 $readme = "$cache/${package_name}-readme.txt";
@@ -35,13 +35,13 @@ $filename = "$cache/$target";
 $filesize = file_exists($filename) ? filesize($filename) : 0;
 if (!$filesize) {
     array_map('unlink',
-	      array_filter((array)glob("$cache/${package_name}-[0-9][0-9][0-9][0-9][0-9][0-9][0-9]*")));
+              array_filter((array)glob("$cache/${package_name}-[0-9][0-9][0-9][0-9][0-9][0-9][0-9]*")));
     $ch = live_curl("https://melpa.org/packages/${target}", $filename);
     $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     if ($status >= 400 || curl_error($ch)) {
-	unlink($filename);
-	header("HTTP/1.0 " . $status . " " . curl_errno($ch));
-	exit();
+        unlink($filename);
+        header("HTTP/1.0 " . $status . " " . curl_errno($ch));
+        exit();
     }
     curl_close($ch);
     $filesize = file_exists($filename) ? filesize($filename) : 0;
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo apt update -yq
 sudo apt install -yq emacs rsync xauth pandoc
 git clone -depth 1 https://github.com/melpa/melpa.git
-mv ~/stack/nginx/html/index.html ~/stack/nginx/html/index.html.orig 
+mv ~/stack/nginx/html/index.html ~/stack/nginx/html/index.html.orig
+# git-scm.com/book/en/v2/Git-on-the-Server-Setting-Up-the-Server
